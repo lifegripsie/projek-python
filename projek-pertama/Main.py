e262f68e3dd1bfc3aa2e445ccb8767c277f16863
lifegripsie <loidggwp9@gmail.com>
2026-08-21 02:40:30 -0700

    Add files via upload

---
+++ projek-pertama/Main.py
@@ -0,0 +1,22 @@
+#PROJEKPERTAMAPYTHON
+#BY RICHARD
+#PROJEK KONVERSI SUHU
+
+print ("----PROGRAM KONVERSI SUHU----")
+
+nama = input ("silakan masukan nama=")
+
+
+suhu = float(input("MASUKAN SUHU DLM CELCIUS:"))
+print ("suhu adalah=",suhu,("Celcius"))
+
+reamur = (4/5) * suhu
+print ("suhu dalam reamur=",reamur,("Reamur"))
+
+fahrenheit = (9/5 * (suhu)) + 32
+print ("suhu dalam fahrenheit=",fahrenheit,("Fahrenheit"))
+
+kelvin = suhu + 273
+print ("suhu dalam kelvin=",kelvin,("Kelvin"))
+
+print ("TERIMAKASIH",nama,("^_^"))
---
+++ projek-pertama/README.md
@@ -0,0 +1,17 @@
+# Project Python Pertama
+
+Program sederhana untuk mengkonversi suhu dari Celsius ke Reamur, Fahrenheit, dan Kelvin.
+
+## Materi yang Digunakan
+
+- input()
+- float()
+- variabel
+- operasi aritmatika
+- print()
+
+## Konversi
+
+- Celsius → Reamur
+- Celsius → Fahrenheit
+- Celsius → Kelvin
